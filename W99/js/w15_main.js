4e34cfd5bdb8b93c2c495f0c41e6c48405ee8dc0
Kento Watanabe <ywmgm086@gmail.com>
2018-05-30 13:16:29 +0900

    a

--- W99/js/w15_main.js
+++ W99/js/w15_main.js
@@ -24,4 +24,46 @@ function main()
     });
     
     screen.loop();
+    
+    var sampleSquare = function() {
+  this.color = "#ff0000";
+  this.isovalue = 128;
+  this.apply = function(){
+    screen.scene.remove( surfaces );
+    surfaces = Isosurfaces( volume, isovalue, Color );
+    screen.scene.add( surfaces );
+	}
+  this.Lambertian = function(){
+    screen.scene.remove( light );
+  }
+  this.Phong = function(){
+    screen.scene.add( light );
+
+    var geometry = new THREE.Geometry();
+    var material = new THREE.ShaderMaterial({
+        vertexColors: THREE.VertexColors,
+        vertexShader: document.
+            getElementById('gouraud2.vert').text,
+        fragmentShader: document.getElementById('gouraud2.frag').text,
+	uniforms: {
+	    light_position: {type: 'v3', value: light.position}
+	}
+    });
+  }
+  this.Box = true;
+};    
+    
+    
+    window.onload = function() {
+    square = new sampleSquare();
+    var gui = new dat.GUI();
+    gui.addColor(square, 'color').onChange(setValue);
+    gui.add(square, 'isovalue', 0, 255).step(1).onChange(setValue);
+    gui.add(square, 'apply');
+    gui.add(square, 'Lambertian');
+    gui.add(square, 'Phong');
+    gui.add(square, 'Box').onChange(setValue);
+    };
+    function setValue() {
+    }
 }
